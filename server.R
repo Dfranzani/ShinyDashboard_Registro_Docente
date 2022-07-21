87e7d3429f6bdc445d8f0dd9efef737d387556de
Dfranzani <dfranzani@gmail.com>
2022-07-20 22:46:40 -0400

    Estructura

--- server.R
+++ server.R
@@ -1,5 +1,7 @@
 server = function(input, output, session){
   
+  # Conexión a Google Drive y carga de bases de datos
+  
   # Set authentication token to be stored in a folder called `.secrets`
   # options(gargle_oauth_cache = ".secrets")
   
@@ -16,6 +18,27 @@ server = function(input, output, session){
   # Authenticate using token. If no browser opens, the authentication works.
   gs4_auth(cache = ".secrets", email = "dfranzani@gmail.com")
   
-  df = read_sheet("https://docs.google.com/spreadsheets/d/1yFn50Y5uZbAVLEuqnYdxAvNVfObA52Tcm4wCeKvzzFM/edit#gid=0")
-  # df = gs4_get("https://docs.google.com/spreadsheets/d/1yFn50Y5uZbAVLEuqnYdxAvNVfObA52Tcm4wCeKvzzFM/edit#gid=0")
+  listado.cursos = as.data.frame(read_sheet("https://docs.google.com/spreadsheets/d/140kK9DxWKtgCtwSxtt3siqNtvTXBX5LlJgm8CB5UuwU/edit#gid=0",
+                              sheet = "Listado"))
+  
+  # Contenido de todas las notas por profesor, en todos los aspectos
+  
+  # urls = list("https://docs.google.com/spreadsheets/d/1OBmwY66WNfIv63GMsBF-gDf9xob0q1lXATQzZsQzwMw/edit#gid=1752874311",
+  #             "https://docs.google.com/spreadsheets/d/1JLqReXrK7QtoamKSyWhfcSsUh4x7RdCXHm3kuVGJ2zM/edit#gid=1752874311",
+  #             "https://docs.google.com/spreadsheets/d/1K8Z_802i5DrLc9CbgmWW6AdBjJTWUVnhhtbqU-V5upI/edit#gid=1752874311",
+  #             "https://docs.google.com/spreadsheets/d/1cbQgCwhI0ZoFQ1gAQqzYhiLvHH53gu2bxgOWjbXx-P8/edit#gid=1752874311")
+  # 
+  # docs = lapply(urls, function(url){
+  #   doc = gs4_get(url)
+  #   nombre = doc$name # nombre del archivo
+  #   hojas = doc$sheets$name # nombre de las hojas
+  #   doc = lapply(as.list(hojas), function(hoja){
+  #     # Las ponderaciones y controles siempre estarán en las primeras 2 columnas, el resto corresponde a pruebas
+  #     return(as.data.frame(read_sheet(url, sheet = hoja)))
+  #   })
+  #   return(list("Nombre" = nombre, "Hoja" = hojas, "Documentos" = doc))
+  # }) 
+
+
+ 
 }
--- ui.R
+++ ui.R
@@ -11,12 +11,8 @@ library(ggpubr) # agrupar gráficos de ggplot con una sola leyenda
 # library(plotly) # gráficos interactivos (se puede combinar con ggplot fácilmente)
 library(googlesheets4) # Conección a la cuenta de Google Drive
 library(Cairo) # mejora resolución y renderizado de gráficos
+library(shinyjs)
 options(shiny.usecairo = T)
-
-
-########################### Datos del panel ###########################
-
-# Datos de los filtros (BBDD)
 
 ########################### Elementos generales del panel ###########################
 
@@ -36,25 +32,58 @@ headerpanel = dashboardHeader(title = "U.F.M.E.",
 sidebar = dashboardSidebar(
   collapsed = FALSE,
   sidebarMenu(
-    menuItem("Registros", tabName = "registros", icon = icon("dashboard", lib = "glyphicon"))
+    menuItem("Profesores", tabName = "registros", icon = icon("dashboard", lib = "glyphicon")),
+    menuItem("Asignaturas", tabName = "asignaturas", icon = icon("dashboard", lib = "glyphicon"))
   )
 )
 
 # Incluir filtros en el sidebar menu
 
-########################### Elementos de paneles: Hoja 1 ###########################
+########################### Elementos de paneles: Hoja 1 y 2 ###########################
 
-########################### Distribución de paneles: Hoja 1 ###########################
+########################### Distribución de paneles: Hoja 1 y 2 ###########################
 
-all = fluidPage()
+# Como la cantidad de profesor es baja podemos crear los paneles manualmente, pero lo ideal es automatizar
+# las pestañas de con insertabpanel
+
+all1 = fluidPage(
+  fluidRow(
+    box(id = "Profesor1", width = 12, solidHeader = TRUE, collapsed = TRUE, collapsible = TRUE,
+        title = "Profesor 1",
+        tabBox(id = "Resumen1", width = 12,
+              tabPanel(id = "p1r1.1", title = "Resumen General"),
+              tabPanel(id = "p1r1.2", title = "Curso 1"),
+              tabPanel(id = "p1r1.3", title = "Curso 2")
+              # Solo rellenar con tablas
+              )
+        ),
+    box(id = "Profesor2", width = 12, solidHeader = TRUE, collapsed = TRUE, collapsible = TRUE,
+        title = "Profesor 2",
+        tabBox(id = "Resumen2", width = 12,
+               tabPanel(id = "p2r2.1", title = "Resumen General"),
+               tabPanel(id = "p2r2.2", title = "Curso 1"),
+               tabPanel(id = "p2r2.3", title = "Curso 2")
+               )
+        )
+  )
+)
+all2 = fluidPage(
+  fluidRow(
+    box(id = "Resumen_asigs", width = 12, solidHeader = TRUE, collapsed = FALSE, collapsible = TRUE,
+        title = "Resumen de Asignaturas")
+    # Añadir resumen de la signatura por profesor y uno general (todo en la misma tabla y añadir gráfico)
+  )
+)
 
 ########################### Despliegue ###########################
 
 # Primera hoja
-hoja1 = tabItem(tabName = "registro", all)
+hoja1 = tabItem(tabName = "registros", all1)
+# Segunda hoja
+hoja2 = tabItem(tabName = "asignaturas", all2)
 
 # Cuerpo de todas las hojas
-bodypanel = dashboardBody(tabItems(hoja1))
+bodypanel = dashboardBody(tabItems(hoja1, hoja2))
 
 # Panel completo
 ui = dashboardPage(header = headerpanel, sidebar = sidebar, body = bodypanel, skin = "black")
